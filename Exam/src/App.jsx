bd901a8a55108a16d0317d92be5cbdaad1191cbf
LeandreGarcia <leandre.gar@gmail.com>
2024-05-28 14:06:01 +0200

    Partie Leandre

--- Exam/src/App.jsx
+++ Exam/src/App.jsx
@@ -1,17 +1,72 @@
-import { useState } from 'react'
-import './App.css'
-import BasketPage from './components/BasketPage'
+import './App.css';
+import React, { useEffect } from 'react';
+import HomePage from './components/HomePage';
+import UserPage from './components/UserPage';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { useDispatch, useSelector, Provider } from 'react-redux';
+import { fetchProducts } from './slices/productsSlice';
+
+import styled from 'styled-components';
+import store from './store';
+
+
+
+const AppContainer = styled.div`
+  background-color: ${props => props.theme.background};
+  color: ${props => props.theme.color};
+  min-height: 100vh;
+  padding: 20px;
+`;
+
+const NavBar = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  background-color: #f0c040;
+`;
+
+const NavLinks = styled.div`
+  a {
+    margin: 0 10px;
+    text-decoration: none;
+    color: inherit;
+  }
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dispatch = useDispatch();
+
+  const totalQuantity = useSelector((state) => state.cart.totalQuantity);
+  const user = useSelector((state) => state.user);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
 
   return (
-    <>
-      <div>
-        <BasketPage/>
-       </div>
-    </>
-  )
+    <Provider store={store}>
+        <Router>
+          <AppContainer>
+            <NavBar>
+              <Link to="/">
+                <h1>EZ Shopping</h1>
+              </Link>
+              <NavLinks>
+                <Link to="/user">{user.name}</Link>
+                <span>{totalQuantity} articles</span>
+              </NavLinks>
+            </NavBar>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/user" element={<UserPage />} /> 
+            </Routes>
+          </AppContainer>
+        </Router>
+      
+    </Provider>
+  );
 }
 
-export default App
+export default App;
+
---
+++ Exam/src/slices/cartSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [],
+    totalQuantity: 0
+  },
+  reducers: {
+    addToCart: (state, action) => {
+      const item = state.items.find(item => item.id === action.payload.id);
+      if (item) {
+        item.quantity += action.payload.quantity;
+      } else {
+        state.items.push(action.payload);
+      }
+      state.totalQuantity += action.payload.quantity;
+    },
+    removeFromCart: (state, action) => {
+      const itemIndex = state.items.findIndex(item => item.id === action.payload.id);
+      if (itemIndex >= 0) {
+        state.totalQuantity -= state.items[itemIndex].quantity;
+        state.items.splice(itemIndex, 1);
+      }
+    }
+  }
+});
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
+export default cartSlice.reducer;
