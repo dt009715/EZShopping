383d4f52e6468ad7b46f854cf2271c903f15a531
dt009715 <80246805+dt009715@users.noreply.github.com>
2024-05-29 17:16:06 +0200

    Update HomePage.jsx

--- Exam/src/components/HomePage.jsx
+++ Exam/src/components/HomePage.jsx
@@ -1,8 +1,7 @@
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import { addToCart } from "../components/slice/cartSlice";
-import React, { useEffect, useState } from , { useEffect, useState };
-from "react";
 
 const ProductList = styled.div`
   display: flex;
