d034d4c16c8286ea2ba0494c8a1b8510df558cbd
LeandreGarcia <leandre.gar@gmail.com>
2024-05-29 12:07:13 +0200

    Update cartSlice.js

--- Exam/src/slices/cartSlice.js
+++ Exam/src/slices/cartSlice.js
@@ -1,14 +1,14 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [],
-    totalQuantity: 0
+    totalQuantity: 0,
   },
   reducers: {
     addToCart: (state, action) => {
-      const item = state.items.find(item => item.id === action.payload.id);
+      const item = state.items.find((item) => item.id === action.payload.id);
       if (item) {
         item.quantity += action.payload.quantity;
       } else {
@@ -17,26 +17,45 @@ const cartSlice = createSlice({
       state.totalQuantity += action.payload.quantity;
     },
     removeFromCart: (state, action) => {
-      const itemIndex = state.items.findIndex(item => item.id === action.payload.id);
-      if (itemIndex >= 0) {
-        state.totalQuantity -= state.items[itemIndex].quantity;
-        state.items.splice(itemIndex, 1);
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      if (itemIndex !== -1) {
+        const item = state.items[itemIndex];
+
+        if (state.totalQuantity > 1) {
+          state.totalQuantity -= item.quantity;
+        }
+        if (state.totalQuantity <= 0) {
+          state.items.splice(itemIndex, 1);
+          state.totalQuantity = 0;
+        }
       }
     },
     updateQuantity: (state, action) => {
-        const { id, quantity } = action.payload;
-        const item = state.items.find((item) => item.id === id);
-        if (item) {
-          item.quantity = quantity;
-        }
-      },
-      clearCart: (state) => {
-        state.items = [];
-      },
+      const { id, quantity } = action.payload;
+      const item = state.items.find((item) => item.id === id);
+      if (item) {
+        item.quantity = quantity;
+      }
     },
-  });
+    resetValue: (state, action) => {
+      state.totalQuantity = 0;
+    },
+    clearCart: (state) => {
+      state.items = [];
+    },
+  },
+});
 
-export const { addToCart, removeFromCart, updateQuantity, clearCart } = cartSlice.actions;
+export const {
+  addToCart,
+  removeFromCart,
+  updateQuantity,
+  clearCart,
+  resetValue,
+} = cartSlice.actions;
 export default cartSlice.reducer;
 
 
